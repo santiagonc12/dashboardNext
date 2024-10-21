fedbd94c3239172c0853d85fa30990060e01f134
Santiago Novoa <santiago.novoa@aviatecnologia.com>
2024-10-21 16:12:27 -0500

    NEW::Dashboard:Nueva estructura de archivos para el enrutamiento de la pagina

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-import '@/app/ui/global.css';
-import { montserrat } from './ui/fonts';
+import "@/app/ui/global.css";
+import { montserrat } from "./ui/fonts";
 export default function RootLayout({
   children,
 }: {
@@ -7,7 +7,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${montserrat.className} antialiased`}>{children}</body>
+      <body className={`${montserrat.className} antialiased`}>
+        {children}
+        <footer className="bg-red-400 w-full h-[30px]">
+          Hecho por mi jejejej
+        </footer>
+      </body>
     </html>
   );
 }
